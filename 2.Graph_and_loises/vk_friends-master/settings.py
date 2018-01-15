6b3a6e6ef31cd215854ed381f1c9131451ce3cd5
Filipp <filfonul@gmail.com>
2018-01-16 00:52:19 +0300

    пкуфцй

--- 2.Graph_and_loises/vk_friends-master/settings.py
+++
@@ -1,9 +0,0 @@
-api_v = '5.21'
-token = ('',
-		)
-my_id = '' 
-deep = 2
-broker = {'user': '', 'password':'', 'ip':'', 'vhost':'vk_friends'}
-backend = broker
-m_friends_dct = {'file': 'mutual_friends_dct', 'graph':'mutual_friends_g'}
-d_friends_dct = {'file': 'deep_friends_dct', 'graph': 'deep_friends_g'}
